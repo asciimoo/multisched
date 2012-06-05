f495fe311b0f278f49942892560d6eb9ba219ce1
asciimoo <asciimoo@gmail.com>
2012-06-05 11:22:09 +0200

    [enh] setup.py added

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='multisched',
+    version='0.1.3',
+    author='Adam Tauber <asciimoo>',
+    author_email='asciimoo@faszkorbacs.hu',
+    packages=find_packages(),
+    scripts=['multisched.py'],
+    py_modules=['multisched' ],
+    keywords='threaded scheduler',
+    url='http://pypi.python.org/pypi/multisched/',
+    license='AGPLv3+',
+    description='',
+    long_description=open('README.markdown').read(),
+    classifiers = ["Development Status :: 4 - Beta",
+                   "License :: OSI Approved :: GNU Affero General Public License v3",
+    ],
+
+)
